221277908deeabc5444df2999b1d208d6e630549
Aditya Raj <aditya.x510@gmail.com>
2020-03-26 22:35:54 +0530

    init

---
+++ sw.js
@@ -0,0 +1,15 @@
+// installing service worker
+self.addEventListener('install', event => {
+	console.log('service worker has been installed ');
+});
+
+// activate service worker
+self.addEventListener('activate', event => {
+	console.log('service worker has been activated');
+});
+
+// fetch event
+
+self.addEventListener('fetch', event => {
+	console.log('fetch event fired', event);
+});
