91464ccdfd275b2f5b4c4abbe7717cf8bf60e468
Aditya Raj <aditya.x510@gmail.com>
2020-03-27 14:45:31 +0530

    init

--- sw.js
+++ sw.js
@@ -1,6 +1,30 @@
-// installing service worker
+// methods for offline caching
+// caches.open
+// cache.all and cache.add('./index.html')
+
+const staticCacheName = 'site-static';
+const assets = [
+	'/',
+	'/index.html',
+	'/js/app.js',
+	'/css/bootstrap.css',
+	'/css/style.css',
+	'/css/bootstrap.js',
+	'/css/popper.min.js',
+	'/css/tooltip.min.js',
+	'/css/jquery-3.4.0.slim.min.js',
+	'/icon-384x384.png'
+];
+
+// install event
 self.addEventListener('install', event => {
-	console.log('service worker has been installed ');
+	console.log('service worker installed');
+	event.waitUntil(
+		caches.open(staticCacheName).then(cache => {
+			console.log('caching shell assets');
+			cache.addAll(assets);
+		})
+	);
 });
 
 // activate service worker
@@ -9,6 +33,12 @@ self.addEventListener('activate', event => {
 });
 
 // fetch event
+// when we want to use button to add on home screen
 self.addEventListener('fetch', event => {
 	console.log('fetch event fired', event);
+	event.respondWith(
+		caches.match(event.request).then(cacheRes => {
+			return cacheRes || fetch(event.request);
+		})
+	);
 });
